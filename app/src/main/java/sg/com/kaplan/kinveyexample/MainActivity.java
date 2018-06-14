4f7732757ce59e02a946500238b56ac2346c7365
William Ku <kucheech@gmail.com>
2018-06-14 21:46:53 +0800

    added user login

--- app/src/main/java/sg/com/kaplan/kinveyexample/MainActivity.java
+++ app/src/main/java/sg/com/kaplan/kinveyexample/MainActivity.java
@@ -3,15 +3,23 @@ package sg.com.kaplan.kinveyexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.kinvey.android.Client;
 import com.kinvey.android.callback.KinveyPingCallback;
+import com.kinvey.android.model.User;
+import com.kinvey.android.store.UserStore;
+import com.kinvey.java.core.KinveyClientCallback;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Client mKinveyClient;
+    private Client myKinveyClient;
     TextView status;
 
     @Override
@@ -23,9 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
         String app_key = "kid_BJoViSaxX";
         String app_secret = "c8ab7f0a7b01477593968345e76a1a0a";
-        mKinveyClient = new Client.Builder(app_key, app_secret, this).build();
+        myKinveyClient = new Client.Builder(app_key, app_secret, this).build();
 
-        mKinveyClient.ping(new KinveyPingCallback() {
+        myKinveyClient.ping(new KinveyPingCallback() {
             public void onFailure(Throwable t) {
                 Toast.makeText(MainActivity.this, "Kinvey Ping Failed", Toast.LENGTH_SHORT).show();
                 status.setText("Kinvey Ping Failed");
@@ -35,9 +43,35 @@ public class MainActivity extends AppCompatActivity {
                 status.setText("Kinvey Ping Success");
             }
         });
+        final EditText mEditUserName = (EditText) findViewById(R.id.editTextUser);
+        final EditText mEditPassword = (EditText) findViewById(R.id.editTextPassword);
+        Button button = (Button) findViewById(R.id.buttonLogin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    UserStore.login(mEditUserName.getText().toString(), mEditPassword.getText().toString(), getKinveyClient(), new KinveyClientCallback<User>() {
+                        @Override
+                        public void onFailure(Throwable t) {
+                            CharSequence text = "Wrong username or password.";
+                            Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                        }
+                        @Override
+                        public void onSuccess(User u) {
+                            CharSequence text = "Welcome back," + u.getUsername() + ".";
+                            Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     public Client getKinveyClient(){
-        return mKinveyClient;
+        return myKinveyClient;
     }
+
+
 }
